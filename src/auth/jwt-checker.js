8634e254fb38fd24a4546ba084de0ba52903a2b6
anshu7sah <anshu7sah@gmail.com>
2023-04-24 04:04:52 +0545

    update address for checkout form

--- src/auth/jwt-checker.js
+++ src/auth/jwt-checker.js
@@ -2,7 +2,7 @@ const { auth } = require("express-oauth2-jwt-bearer");
 
 const jwtCheck = auth({
   audience: process.env.AUTH0_AUDIENCE,
-  issuerBaseURL: process.env.AUTH0_ISSUERBASEUR,
+  issuerBaseURL: process.env.AUTH0_ISSUERBASEURL,
   tokenSigningAlg: "RS256",
 });
 
--- src/routes/userRouter.js
+++ src/routes/userRouter.js
@@ -1,8 +1,22 @@
 const express = require("express");
-const { createUser } = require("../controllers/userController.js");
+const {
+  createUser,
+  fetchById,
+  getUserById,
+  updateAddress,
+  getUserAddress,
+} = require("../controllers/userController.js");
 const { jwtCheck } = require("../auth/jwt-checker.js");
 const router = express.Router();
 
 router.post("/users", jwtCheck, createUser);
+
+router.param("id", fetchById);
+
+router.get("/users/:id", jwtCheck, getUserById);
+
+router.put("/users/:id/address", jwtCheck, updateAddress);
+
+router.get("/users/:id/address", jwtCheck, getUserAddress);
 
 module.exports = router;
